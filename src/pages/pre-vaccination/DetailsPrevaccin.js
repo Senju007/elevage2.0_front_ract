50781585b0c0d8852d5ef11ee573dcb7f69f988b
Senju007-sudo <radamanirinasitraka@gmail.com>
2021-10-05 09:01:34 +0200

    "modification front"

--- src/pages/pre-vaccination/DetailsPrevaccin.js
+++ src/pages/pre-vaccination/DetailsPrevaccin.js
@@ -14,7 +14,6 @@ import PrevaccinDataService from '../../services/PrevaccinServices';
 import ElevageDataService from '../../services/ElevageServices';
 import {
   CalendarToday,
-  PermIdentity,
 } from "@material-ui/icons";
 import { Icon } from '@iconify/react';
 import "./prevaccin.css";
@@ -31,11 +30,11 @@ const Prevaccin = props => {
   const initialPrevaccinState = {
     id: null,
     elevage: "",
-    nom:"",
+    nom: "",
     date_debut: "",
     date_fin: "",
     prix_unitaire: "",
-    prix_total:"",
+    prix_total: "",
     etat: "",
     description: "",
   };
@@ -51,13 +50,13 @@ const Prevaccin = props => {
   const handleChangeEtat = (event) => {
     const etat = event.target.etat;
     setCurrentPrevaccin({ ...currentPrevaccin, etat: etat });
-    setCurrentPrevaccin({ ...currentPrevaccin, etat : event.target.value  });
+    setCurrentPrevaccin({ ...currentPrevaccin, etat: event.target.value });
   };
 
   const handleChangePrix = (event) => {
     const prix_unitaire = event.target.etat;
     setCurrentPrevaccin({ ...currentPrevaccin, prix_unitaire: prix_unitaire });
-    setCurrentPrevaccin({ ...currentPrevaccin, prix_unitaire : event.target.value  });
+    setCurrentPrevaccin({ ...currentPrevaccin, prix_unitaire: event.target.value });
   };
 
 
@@ -102,14 +101,14 @@ const Prevaccin = props => {
   const updatePublished = status => {
     var data = {
       id: currentPrevaccin.id,
-      nom : currentPrevaccin.nom,
-      date_debut : currentPrevaccin.date_debut,
-      date_fin : currentPrevaccin.date_fin,
+      nom: currentPrevaccin.nom,
+      date_debut: currentPrevaccin.date_debut,
+      date_fin: currentPrevaccin.date_fin,
       elevage: currentPrevaccin.elevage,
       prix_unitaire: currentPrevaccin.prix_unitaire,
       etat: currentPrevaccin.etat,
-      prix_total : currentPrevaccin.prix_total,
-      description : currentPrevaccin.description,
+      prix_total: currentPrevaccin.prix_total,
+      description: currentPrevaccin.description,
     };
 
     PrevaccinDataService.update(currentPrevaccin.id, data)
@@ -142,130 +141,127 @@ const Prevaccin = props => {
           <div className="nourritureTitleContainer">
             <h1 className="nourritureTitle">Details </h1>
           </div>
-            <div className="nourritureContainer">
-              <div className="nourritureShow">
-                <div className="nourritureShowTop">
-                  <div className="nourritureShowTopTitle">
-                    <span className="nourritureShowUsername">Prevaccin N: {currentPrevaccin.id}</span>
-                  </div>
-
+          <div className="nourritureContainer">
+            <div className="nourritureShow">
+              <div className="nourritureShowTop">
+                <div className="nourritureShowTopTitle">
+                  <span className="nourritureShowUsername">Prevaccin N: {currentPrevaccin.id}</span>
                 </div>
 
-                <div className="nourritureShowBottom">
-                  <span className="nourritureShowTitle">Details</span>
-                  <div className="nourritureShowInfo">
-                  <Icon icon={needleIcon} />
-                    <span className="nourritureShowInfoTitle">Nom : {currentPrevaccin.nom}</span>
-                  </div>
-                  <div className="nourritureShowInfo">
-                  <Icon icon={stateIcon} />
-                    <span className="nourritureShowInfoTitle">Etat : {currentPrevaccin.etat}</span>
-                  </div>
-                  <div className="nourritureShowInfo">
-                    <CalendarToday className="nourritureShowIcon" />
-                    <span className="nourritureShowInfoTitle">Debut : {currentPrevaccin.date_debut}</span>
-                  </div>
-                  <div className="nourritureShowInfo">
-                    <CalendarToday className="nourritureShowIcon" />
-                    <span className="nourritureShowInfoTitle">Fin : {currentPrevaccin.date_fin}</span>
-                  </div>
-                  <div className="nourritureShowInfo">
-                    <Icon icon={animalChicken} />
-                    <span className="nourritureShowInfoTitle">Elevage N : {currentPrevaccin.elevage}</span>
-                  </div>
-                  <span className="nourritureShowTitle">Plus de details</span>
-                  
-                  <div className="nourritureShowInfo">
-                    <Icon icon={textDescription24Regular}/>
-                    <span className="nourritureShowInfoTitle">Description : {currentPrevaccin.description}</span>
-                  </div>
-                  <div className="nourritureShowInfo">
-                  <Icon icon={cashIcon}/>
-                    <span className="nourritureShowInfoTitle">Prix unitaire : {currentPrevaccin.prix_unitaire} </span>
-                  </div>
-                  <div className="nourritureShowInfo">
-                  <Icon icon={cashIcon}/>
-                    <span className="nourritureShowInfoTitle">Prix total : {currentPrevaccin.prix_unitaire*nbpoulet} </span>
-                  </div>
-                </div>
               </div>
 
-
-              <div className="nourritureUpdate">
-                <span className="nourritureUpdateTitle">Modifier</span>
-                <form className="nourritureUpdateForm">
-                  <div className="nourritureUpdateLeft">
-                    <div className="nourritureUpdateItem">
-                       <label htmlFor="cin">Prix unitaire (Ar)</label>
-                        <NumberFormat 
-                        thousandsGroupStyle="thousand"
-                        value={currentPrevaccin.prix_unitaire}
-                        prefix="Ar "
-                        decimalSeparator = "."
-                        displayType="input"
-                        type="text"
-                        thousandSeparator={true}
-                        allowNegative={false}
-                        onValueChange={(values) => {
-                          const {formattedValue , value} = values
-                          setCurrentPrevaccin({ ...currentPrevaccin, prix_unitaire: value , prix_total : value*nbpoulet });
-                        }}
-                     />
-                   </div>
-
-                   <div className="nourritureUpdateItem">
-                        <NumberFormat 
-                        thousandsGroupStyle="thousand"
-                        value={currentPrevaccin.prix_total}
-                        prefix="Ar "
-                        decimalSeparator = "."
-                        displayType="input"
-                        type="hidden"
-                        thousandSeparator={true}
-                        allowNegative={false}
-                        onValueChange={(values) => {
-                          const {formattedValue , value} = values
-                          setCurrentPrevaccin({ ...currentPrevaccin, prix_total: value });
-                        }}
-                     />
-                   </div>
-
-                   <div className="nourritureUpdateItem">
-                      <label>Etat</label>
-                      <NativeSelect
-                        value={currentPrevaccin.etat}
-                        onChange={handleChangeEtat}
-                        name="etat"
-                        id="etat"
-                        inputProps={{
-                          type: 'etat',
-                          id: 'name-native-disabled',
-                        }}
-                      >
-                        <option value="">Choisir</option>
-                        <option value="Inachevé">Inachevé</option>
-                        <option value="Achevé">Achevé</option>
-                      </NativeSelect>
-
-
-                      
-
-          </div>
-                    
-
-
-                  </div>
-                  
-                </form>
-
-                <div className="nourritureUpdateRight">
-                <Button className="nourritureAddButton" variant="contained" color="success" onClick={updatePrevaccin}>
-                     Modifier
-                </Button>
+              <div className="nourritureShowBottom">
+                <span className="nourritureShowTitle">Details</span>
+                <div className="nourritureShowInfo">
+                  <Icon icon={needleIcon} />
+                  <span className="nourritureShowInfoTitle">Nom : {currentPrevaccin.nom}</span>
                 </div>
-                <p>{message}</p>
+                <div className="nourritureShowInfo">
+                  <Icon icon={stateIcon} />
+                  <span className="nourritureShowInfoTitle">Etat : {currentPrevaccin.etat}</span>
+                </div>
+                <div className="nourritureShowInfo">
+                  <CalendarToday className="nourritureShowIcon" />
+                  <span className="nourritureShowInfoTitle">Debut : {currentPrevaccin.date_debut}</span>
+                </div>
+                <div className="nourritureShowInfo">
+                  <CalendarToday className="nourritureShowIcon" />
+                  <span className="nourritureShowInfoTitle">Fin : {currentPrevaccin.date_fin}</span>
+                </div>
+                <div className="nourritureShowInfo">
+                  <Icon icon={animalChicken} />
+                  <span className="nourritureShowInfoTitle">Elevage N : {currentPrevaccin.elevage}</span>
+                </div>
+                <span className="nourritureShowTitle">Plus de details</span>
+
+                <div className="nourritureShowInfo">
+                  <Icon icon={textDescription24Regular} />
+                  <span className="nourritureShowInfoTitle">Description : {currentPrevaccin.description}</span>
+                </div>
+                <div className="nourritureShowInfo">
+                  <Icon icon={cashIcon} />
+                  <span className="nourritureShowInfoTitle">Prix unitaire : {currentPrevaccin.prix_unitaire} </span>
+                </div>
+                <div className="nourritureShowInfo">
+                  <Icon icon={cashIcon} />
+                  <span className="nourritureShowInfoTitle">Prix total : {currentPrevaccin.prix_unitaire * nbpoulet} </span>
+                </div>
               </div>
             </div>
+
+
+            <div className="nourritureUpdate">
+              <span className="nourritureUpdateTitle">Modifier</span>
+              <form className="nourritureUpdateForm">
+                <div className="nourritureUpdateLeft">
+                  <div className="nourritureUpdateItem">
+                    <label htmlFor="cin">Prix unitaire (Ar)</label>
+                    <NumberFormat
+                      thousandsGroupStyle="thousand"
+                      value={currentPrevaccin.prix_unitaire}
+                      prefix="Ar "
+                      decimalSeparator="."
+                      displayType="input"
+                      type="text"
+                      thousandSeparator={true}
+                      allowNegative={false}
+                      onValueChange={(values) => {
+                        const { formattedValue, value } = values
+                        setCurrentPrevaccin({ ...currentPrevaccin, prix_unitaire: value, prix_total: value * nbpoulet });
+                      }}
+                    />
+                  </div>
+
+                  <div className="nourritureUpdateItem">
+                    <NumberFormat
+                      thousandsGroupStyle="thousand"
+                      value={currentPrevaccin.prix_total}
+                      prefix="Ar "
+                      decimalSeparator="."
+                      displayType="input"
+                      type="hidden"
+                      thousandSeparator={true}
+                      allowNegative={false}
+                      onValueChange={(values) => {
+                        const { formattedValue, value } = values
+                        setCurrentPrevaccin({ ...currentPrevaccin, prix_total: value });
+                      }}
+                    />
+                  </div>
+
+                  <div className="nourritureUpdateItem">
+                    <label>Etat</label>
+                    <NativeSelect
+                      value={currentPrevaccin.etat}
+                      onChange={handleChangeEtat}
+                      name="etat"
+                      id="etat"
+                      inputProps={{
+                        type: 'etat',
+                        id: 'name-native-disabled',
+                      }}
+                    >
+                      <option value="">Choisir</option>
+                      <option value="Inachevé">Inachevé</option>
+                      <option value="Achevé">Achevé</option>
+                    </NativeSelect>
+
+
+
+
+                  </div>
+                </div>
+
+              </form>
+
+              <div className="nourritureUpdateRight">
+                <Button className="nourritureAddButton" variant="contained" color="success" onClick={updatePrevaccin}>
+                  Modifier
+                </Button>
+              </div>
+              <p>{message}</p>
+            </div>
+          </div>
         </div>
       ) : (
         <div>
